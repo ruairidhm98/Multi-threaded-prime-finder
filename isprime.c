66e6c80c508b6d10510f47fb4df61dda6babd200
“Ruairidh <2250079m@student.gla.ac.uk>
2018-11-20 23:41:22 +0000

    Bug found

--- isprime.c
+++ isprime.c
@@ -1,3 +1,5 @@
+#include <limits.h>
+#include <math.h>
 #include "isprime.h"
 
 /*
@@ -5,14 +7,20 @@
  */
 
 int is_prime(unsigned long number) {
-   unsigned long tNum;
-   unsigned long tLim;
+   
+   unsigned long tNum, tLim, result, a;
 
    if (number == 1 || number == 2)
       return 1;
    if ((number % 2) == 0)
       return 0;
-   for (tNum=3, tLim=number; tLim > tNum; tLim = number / tNum, tNum += 2) {
+   
+   a = 6;
+   result = pow((double) a, (double) number);
+   if (result < ULONG_MAX) 
+       if ((result - a) % number == 0) return 1;
+   
+   for (tNum = 3, tLim = number; tLim > tNum; tLim = number / tNum, tNum += 2) {
       if ((number % tNum) == 0)
          return 0;
    }
